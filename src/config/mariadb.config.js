b16a87575458cd16a093de7fc7eb87ca1f31da9e
KDM_korea <kdm_korea@naver.com>
2021-02-01 10:12:53 +0900

    Feat: 데이터베이스 연결 코드

---
+++ src/config/mariadb.config.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const basename = path.basename(__filename);
+const db = {};
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mariadb',
+    logging: false,
+  }
+);
+
+const modelDir = path.join(__dirname, '../', './models/');
+
+fs.readdirSync(modelDir)
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
+  })
+  .forEach((file) => {
+    const model = sequelize.import(path.join(modelDir, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
