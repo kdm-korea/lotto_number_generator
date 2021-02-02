cc1f8982c1611a6ef3f2fe766f39cf3e7ea87dfe
KDM_korea <kdm_korea@naver.com>
2021-02-02 10:15:23 +0900

    Feat: 유저 테이블

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nickNmae: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      hash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
+    }
+  );
+
+  return User;
+};
