ba02f93f1a5424d99f1f1a6ed7fe1be3d39f599d
KDM_korea <kdm_korea@naver.com>
2021-02-03 12:21:52 +0900

    Delete: 테스트겸 사용하던 라우터 삭제

--- src/router.js
+++ src/router.js
@@ -1,14 +1,5 @@
 import express from 'express';
-import a from './component/lottoCrawling';
 
 const app = express();
-
-app.get('/', async (req, res) => {
-  const lotto = await a();
-
-  res.send(
-    `${lotto[0]}회차 로또 당첨번호는 ${lotto[1]} ${lotto[2]} ${lotto[3]} ${lotto[4]} ${lotto[5]} ${lotto[6]} + ${lotto[7]}`
-  );
-});
 
 export default app;
