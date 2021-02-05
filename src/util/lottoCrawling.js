2d28d72c8ff1e11e46324cfbf06f97b5b3384438
KDM_korea <kdm_korea@naver.com>
2021-02-05 20:58:05 +0900

    Fix: 크롤러 반환 데이터 수정

--- src/util/lottoCrawling.js
+++ src/util/lottoCrawling.js
@@ -1,14 +1,9 @@
 import axios from 'axios';
 import NotFoundError from '../component/exception/NotFound.Error';
 
-/**
- * 로또 사이트의 당첨번호 크롤링
- * https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=
+/** 로또 사이트의 당첨번호 크롤링
  * @Request
- * @int 회차
- *
- * @Response
- * @Object {round:int, 1:int, 2:int, 3:int, 4:int, 5:int, 6:int, 7:int}
+ * @param {Number} round
  */
 const crawling = async (round) => {
   const crawl = await axios.get(
@@ -20,16 +15,19 @@ const crawling = async (round) => {
   if (data.returnValue === 'fail') {
     throw new NotFoundError('없는 당첨번호'); //= > 슬랙으로 메시징 처리 404
   }
+  const balls = [
+    data.drwtNo1,
+    data.drwtNo2,
+    data.drwtNo3,
+    data.drwtNo4,
+    data.drwtNo5,
+    data.drwtNo6,
+    data.bnusNo,
+  ];
 
   return {
     round: data.drwNo,
-    1: data.drwtNo1,
-    2: data.drwtNo2,
-    3: data.drwtNo3,
-    4: data.drwtNo4,
-    5: data.drwtNo5,
-    6: data.drwtNo6,
-    7: data.bnusNo,
+    balls,
   };
 };
 
