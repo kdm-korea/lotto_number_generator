f554c06eb1216f9494924b8eb389489fb9a67733
KDM_korea <kdm_korea@naver.com>
2021-02-05 20:56:55 +0900

    Feat: 로또 라우터

---
+++ src/api/lotto/router.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import lottoController from './controller/lotto.controller';
+
+const router = express();
+
+/**
+ * 매주 토요일 21:00 로또번호 추첨 후 LottoScore에 결과를 저장하는 API
+ * 서버에 걸어놓을 예정
+ */
+router.get('/', lottoController.appearLottoWin);
+
+/**
+ * 매주 토요일 24:00 각 알고리즘들을 돌려 각 수의 확률을 계산하는 API
+ * 서버에 걸어놓을 예정
+ */
+// router.post('/');
+
+export default router;
