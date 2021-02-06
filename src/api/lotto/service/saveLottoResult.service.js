ad877ead1b7966da16b39e31562defeb7fbe2906
KDM_korea <kdm_korea@naver.com>
2021-02-06 20:06:39 +0900

    Feat: 예상번호 결과 저장하는 함수 추가

--- src/api/lotto/service/saveLottoResult.service.js
+++ src/api/lotto/service/saveLottoResult.service.js
@@ -1,8 +1,14 @@
 import { Op } from 'sequelize';
 import _ from 'underscore';
-import { LottoBall, LottoRound, PredictLotto } from '../../../models';
+import {
+  LottoBall,
+  LottoRound,
+  LottoScore,
+  PredictLotto,
+} from '../../../models';
+import { calLottoRank } from '../../../util/calLottoRank';
 
-/**
+/** 현재 회차의 예상 로또
  * @param {Number} round 회차
  */
 const findPredictLottoByRound = async (round) => {
@@ -19,11 +25,24 @@ const findPredictLottoByRound = async (round) => {
   });
 };
 
-/**
+const findPredictLottoBallResult = async (predictIds) =>
+  (
+    await PredictLotto.findAll({
+      where: { id: predictIds },
+      attributes: ['lottoScoreId'],
+      include: {
+        model: LottoBall,
+        attributes: ['isCorrect'],
+        right: true,
+      },
+    })
+  ).map((o) => o.get({ raw: true }));
+
+/** 예상 로또 볼 결과 저장
  * @param {Number} predictLottoIds 예상로또 Idx
  * @param {Array<Number>} balls 로또 볼들
  */
-const saveResultBalls = async (predictLottoIds, balls) => {
+const savePredictBallsResult = async (predictLottoIds, balls) => {
   return LottoBall.update(
     {
       isCorrect: true,
@@ -37,6 +56,26 @@ const saveResultBalls = async (predictLottoIds, balls) => {
   );
 };
 
+// 데이터 셀렉트
+const savePredictLottoScore = async (predictIds) => {
+  const predictLottos = await findPredictLottoBallResult(predictIds);
+
+  const scores = predictLottos.map((predictLotto) => {
+    const predictBallresults = _.pluck(
+      predictLotto.LottoBalls.map((lottoBall) => lottoBall.get({ raw: true })),
+      'isCorrect'
+    );
+    return {
+      id: predictLotto.lottoScoreId,
+      ranking: calLottoRank(predictBallresults),
+    };
+  });
+
+  await LottoScore.bulkCreate(scores, {
+    updateOnDuplicate: ['ranking'],
+  });
+};
+
 /** 로또 예상번호 결과 저장
  * @param {Number} round 회차
  * @param {Array<Number>} balls 로또 볼들
@@ -44,7 +83,11 @@ const saveResultBalls = async (predictLottoIds, balls) => {
 const execLottoResult = async (round, balls) => {
   const predictLottos = await findPredictLottoByRound(round);
 
-  await saveResultBalls(_.pluck(predictLottos, 'id'), balls);
+  const predictIds = _.pluck(predictLottos, 'id');
+
+  await savePredictBallsResult(predictIds, balls);
+
+  await savePredictLottoScore(predictIds);
 };
 
 export default execLottoResult;
