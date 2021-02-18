5eea2a174c44c160abbc492b0b8dba887c2eae68
KDM_korea <kdm_korea@naver.com>
2021-02-18 17:46:30 +0900

    Delete: Dao 부분은 모델의 메소드로 이동함

--- src/api/lotto/service/addRound.service.js
+++
@@ -1,9 +0,0 @@
-import { LottoRound } from '../../../models';
-
-/** 다음회차의 당첨번호 추가
- *
- * @param {Number} round 현재 회차번호
- */
-const addLottoRound = async (round) => LottoRound.create({ round: round + 1 });
-
-export default addLottoRound;
--- src/api/lotto/service/currentRound.service.js
+++
@@ -1,8 +0,0 @@
-import { LottoRound } from '../../../models';
-
-/**
- * 현재 로또 회차
- */
-const execCurentRound = async () => LottoRound.max('round');
-
-export default execCurentRound;
