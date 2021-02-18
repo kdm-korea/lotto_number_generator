42df3793dc6717337adbb054d4e5b712e2298f0b
KDM_korea <kdm_korea@naver.com>
2021-02-18 17:53:56 +0900

    Fix: 로또 등수 알고리즘 수정

--- src/util/calLottoRank.js
+++ src/util/calLottoRank.js
@@ -1,11 +1,18 @@
-/** 1 ~ 7등까지 등수
- * @param {Array<boolean>} balls
+import _ from 'lodash';
+
+/** 1 ~ 6등까지 등수를 구하는 함수
+ * @param {Array<number>} predictBalls 예상 로또 볼들
+ * @param {Array<number>} winBalls 당첨된 로또 볼들
  */
-export const calLottoRank = (balls) => {
-  const rank = 8 - balls.reduce((x, y) => x + y);
+export const calLottoRank = (predictBalls, winBalls) => {
+  const rank = _.union(predictBalls, winBalls).length - 5;
 
   if (rank === 2) {
-    return balls[6] === true ? 1 : 2;
+    return predictBalls[predictBalls.length - 1] !==
+      winBalls[winBalls.length - 1]
+      ? 1
+      : 2;
   }
-  return rank > 5 ? 6 : rank;
+
+  return rank > 5 ? -1 : rank;
 };
