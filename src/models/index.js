c3ff666b2af14008fe7f9cb0e379ded30cf4692c
KDM_korea <kdm_korea@naver.com>
2021-02-18 17:50:55 +0900

    Feat: 로또 모델 인덱스

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,6 @@
 export { default as AlgorithmKind } from './AlgorithmKind';
 export { default as LottoBall } from './LottoBall';
 export { default as LottoRound } from './LottoRound';
-export { default as LottoScore } from './LottoScore';
 export { default as PredictLotto } from './PredictLotto';
 export { default as PredictLottoPercent } from './PredictLottoPercent';
 export { default as User } from './User';
