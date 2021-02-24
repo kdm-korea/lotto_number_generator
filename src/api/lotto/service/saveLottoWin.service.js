4a386ec64a0af65f789c1a81d6445c950e7130d7
KDM_korea <kdm_korea@naver.com>
2021-02-24 23:29:30 +0900

    Fix: 인자값 수정

--- src/api/lotto/service/saveLottoWin.service.js
+++ src/api/lotto/service/saveLottoWin.service.js
@@ -1,17 +1,16 @@
 import { LottoBall } from '../../../models';
 
 /** 당첨번호 저장
- * @Object
- * @param {Number} round 현재 회차
  * @param {Array<Number>} balls 로또 볼
+ * @param {Number} round 현재 회차
  */
-const execSaveLottoWin = async (lottoWin) => {
+const execSaveLottoWin = async (balls, round) => {
   await LottoBall.bulkCreate(
-    lottoWin.balls.map((ball, num) => ({
+    balls.map((ball, num) => ({
       num: num + 1,
       ball,
       isCorrect: true,
-      lottoWin: lottoWin.round,
+      lottoWin: round,
     }))
   );
 };
