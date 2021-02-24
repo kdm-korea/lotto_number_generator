691e9fc19940a8b43c0fc3ccea322512c8ecb83c
KDM_korea <kdm_korea@naver.com>
2021-02-24 23:27:59 +0900

    Feat: 알고리즘의 확률을 계산하는 서비스

--- src/api/lotto/service/index.js
+++ src/api/lotto/service/index.js
@@ -1,3 +1,4 @@
 export { default as saveLottoWin } from './saveLottoWin.service';
 export { default as savePredictLottoBallResult } from './savePredictBall.result.service';
 export { default as savePredictLottoRank } from './savePredictLotto.rank.service';
+export { default as calAlgorithm } from './calAlgorithm.service';
