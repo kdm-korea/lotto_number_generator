da21378d3a871bfe4f6ffe60ffcbc6898c45e4ae
KDM_korea <kdm_korea@naver.com>
2021-02-24 23:30:27 +0900

    Feat: 칼만필터 추가

---
+++ src/component/lottoAlgorithm/kalmanFilter.js
@@ -0,0 +1,41 @@
+import { PredictLottoPercent } from '../../models';
+
+// 세타는 회차, 볼은 퍼센트
+const filter = (current, lastest) => {
+  return (
+    current.percent +
+    (lastest.percent - current.percent) *
+      (current.theta ** current.theta /
+        (current.theta ** current.theta + lastest.theta ** lastest.theta))
+  );
+};
+
+/** 칼만필터
+ * @param {Array<PredictLottoPercent>} kalmanPercents
+ * @param {Array<number>} winBalls
+ */
+const execKalmanFilter = async (kalmanPercents, winBalls) => {
+  console.log(winBalls);
+  return Promise.all(
+    kalmanPercents.map((predictPercent) => {
+      const current = {
+        percent:
+          winBalls.find((ball) => ball === predictPercent.ball) === undefined
+            ? 0
+            : 1,
+        theta: 0.5,
+      };
+
+      const lastest = {
+        percent: predictPercent.percent,
+        theta: 0.2,
+      };
+
+      // eslint-disable-next-line no-param-reassign
+      predictPercent.percent = filter(current, lastest);
+      return predictPercent;
+    })
+  );
+};
+
+export default execKalmanFilter;
