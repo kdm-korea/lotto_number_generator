6e0c4cfea28bd2ccaf6f77975b9fa8ac750e9b35
KDM_korea <kdm_korea@naver.com>
2021-02-24 23:28:35 +0900

    Feat: 디비에서 로또 당첨번호를 배열로 반환해주는 서비스

--- src/api/lotto/service/index.js
+++ src/api/lotto/service/index.js
@@ -1,4 +1,5 @@
 export { default as saveLottoWin } from './saveLottoWin.service';
 export { default as savePredictLottoBallResult } from './savePredictBall.result.service';
 export { default as savePredictLottoRank } from './savePredictLotto.rank.service';
+export { default as getLottoBall } from './getLottoBall.service';
 export { default as calAlgorithm } from './calAlgorithm.service';
