3da7805248ad38cbcae731c4220ed29c13dddbcb
KDM_korea <kdm_korea@naver.com>
2021-02-25 17:32:47 +0900

    Feat: User 라우터 추가

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import lotto from './api/lotto/router';
+import user from './api/user/router';
 
 const app = express();
 
 app.use('/lotto', lotto);
+
+app.use('/user', user);
 
 export default app;
