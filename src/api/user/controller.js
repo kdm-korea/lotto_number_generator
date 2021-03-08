d7247eaa2ad2595e44a493fa36633a28142b23ce
KDM_korea <kdm_korea@naver.com>
2021-03-08 23:33:07 +0900

    Feat: 유저 추가 및 구독취소 컨트롤러 추가

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -1,13 +1,25 @@
-import { saveUser } from './service';
+import userSerivce from './service';
 
 /** 계정 생성
  */
 const createAccount = async (req, res, next) => {
-  const { uuid, nickName } = req.params;
+  const { uuid, nickName } = req.query;
 
-  saveUser(uuid, nickName)
+  try {
+    await userSerivce.existUser(uuid);
+    const user = await userSerivce.saveUser(uuid, nickName);
+    res.status(204).json({ user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const cancelSubscribe = async (req, res, next) => {
+  const { uuid } = req.params;
+  userSerivce
+    .cancelSubscribe(uuid)
     .then(() => res.status(204).json())
     .catch((error) => next(error));
 };
 
-export default { createAccount };
+export default { createAccount, cancelSubscribe };
