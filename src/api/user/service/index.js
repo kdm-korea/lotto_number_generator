49b67268c83207704969c723b3fd104aa3f2b217
KDM_korea <kdm_korea@naver.com>
2021-03-09 20:02:41 +0900

    [#1] Feat: 인덱스 추가

--- src/api/user/service/index.js
+++ src/api/user/service/index.js
@@ -1,1 +1,5 @@
-export { default as saveUser } from './saveUser.service';
+import saveUser from './saveUser.service';
+import cancelSubscribe from './cancelSubscribe.service';
+import existUser from './existUser.service';
+
+export default { saveUser, cancelSubscribe, existUser };
