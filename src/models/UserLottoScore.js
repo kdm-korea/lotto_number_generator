59cbf4ebc5cd514a86ff647e74c059c05eb43917
KDM_korea <kdm_korea@naver.com>
2021-03-11 21:21:00 +0900

    [#2] Feat: 유저가 가진 예상로또의 정보와 유저의 정보를 제공하는 메소드

--- src/models/UserLottoScore.js
+++ src/models/UserLottoScore.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import { PredictLotto, User } from '.';
 
 export default class UserLottoScore extends Sequelize.Model {
   static init(sequelize) {
@@ -29,5 +30,28 @@ export default class UserLottoScore extends Sequelize.Model {
     this.belongsTo(models.User);
 
     this.belongsTo(models.PredictLotto);
+  }
+
+  /** 유저가 가진 예상로또의 정보와 유저의 정보를 제공하는 메소드
+   * @param {number} id 예상로또 Id
+   */
+  static async findAllWithUserWithPredictLottoByPredictLottoId(id) {
+    return this.findAll({
+      include: [
+        {
+          model: User,
+          right: true,
+        },
+        {
+          model: PredictLotto,
+          where: {
+            lottoRoundId: id,
+          },
+          right: true,
+        },
+      ],
+      raw: true,
+      nest: true,
+    });
   }
 }
