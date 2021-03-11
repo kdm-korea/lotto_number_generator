ce4be2176dddd83f16d6c24685268dbf5dcbc419
KDM_korea <kdm_korea@naver.com>
2021-03-11 21:20:02 +0900

    Fix: 사용하지 않는 모듈 삭제

--- src/api/lotto/service/calAlgorithm.service.js
+++ src/api/lotto/service/calAlgorithm.service.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 import { kalmanFilter } from '../../../component/lottoAlgorithm';
-import { AlgorithmKind } from '../../../models';
 
 /** 알고리즘별로 확률을 계산하여 반환
  * @param {Array<AlgorithmKind>} algorithmKindsWithPercents 알고리즘들과 알고리즘별 퍼센트
